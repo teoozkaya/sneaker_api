8685e132e5c84e489fc6960fd5453628069a1b46
oezkaya <teoozkaya@gmail.com>
2023-09-19 14:10:17 +0300

    init

---
+++ src/main/java/com/example/sneakers/SneakersApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sneakers/SneakersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sneakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
